e2616a87b792c735503fce4c0f8c0ff24fd921b2
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-03-11 11:19:38 +0100

    updates -  setting up composer

--- setup.sh
+++ setup.sh
@@ -24,3 +24,6 @@ sudo apt-get install php7.0
 sudo apt-get install php-pear php7.0-curl php7.0-dev php7.0-gd php7.0-mbstring php7.0-zip php7.0-mysql php7.0-xml php7.0-pgsql php7.0-sqlite3 php7.0-gd php7.0-curl php7.0-memcached php7.0-imap php7.0-mysql php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-intl php7.0-zip php7.0-readline 
 
 update-alternatives --set php /usr/bin/php7.0
+
+#installing composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
