4e47d7efd64c9bf0906bb632dec3ed14a2d00efa
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-03-11 10:44:42 +0100

    More updates

--- setup.sh
+++ setup.sh
@@ -4,10 +4,23 @@
 
 #setup the ppa
 apt-get install python-software-properties
-add-apt-repository ppa:ondrej/php
+#add-apt-repository ppa:ondrej/php
+#apt-get install -y language-pack-en-base
+#LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
+
+apt-get update &&
+apt-get install -y language-pack-en-base &&
+export LC_ALL=en_US.UTF-8 &&
+export LANG=en_US.UTF-8 &&
+apt-get install -y software-properties-common &&
+add-apt-repository -y ppa:ondrej/php &&
+add-apt-repository -y ppa:ondrej/mariadb-10.0 && 
+apt-get update && 
+apt-get -y upgrade
+
 sudo apt-get update
 
-sudo apt-get install php7.1
-sudo apt-get install php-pear php7.1-curl php7.1-dev php7.1-gd php7.1-mbstring php7.1-zip php7.1-mysql php7.1-xml php7.1-pgsql php7.1-sqlite3 php7.1-gd php7.1-curl php7.1-memcached php7.1-imap php7.1-mysql php7.1-bcmath php7.1-mbstring php7.1-soap php7.1-intl php7.1-zip php7.1-readline 
+sudo apt-get install php7.2
+sudo apt-get install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml php7.2-pgsql php7.2-sqlite3 php7.2-gd php7.2-curl php7.2-memcached php7.2-imap php7.2-mysql php7.2-bcmath php7.2-mbstring php7.2-soap php7.2-intl php7.2-zip php7.2-readline 
 
-update-alternatives --set php /usr/bin/php7.1
+update-alternatives --set php /usr/bin/php7.2
